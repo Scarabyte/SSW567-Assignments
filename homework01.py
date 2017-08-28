b67f65fe8ef2e6a23865f942daf8f1b65cc972bf
Scarabyte <aburbidg@stevens.edu>
2017-08-28 18:08:29 -0400

    Format code to PEP-8 standards
     - Use PyDev's built-in code formatter to apply PEP-8 style guidelines

--- homework01.py
+++ homework01.py
@@ -3,70 +3,75 @@
 #     Homework 01
 #    Adam Burbidge
 #############################
+from math import sqrt
 
-def isTriangle(a,b,c):
+
+def isTriangle(a, b, c):
     """Check that the numbers form a valid triangle.
-    
+
     In order for three numbers (representing the lengths of line segments)
     to form a triangle, the sum of the two smaller numbers must be
     greater than or equal to the largest number.
-    
+
     This can be seen in the Triangle Inequality:
-    
+
         a + b >= c
         a + c >= b
         b + c >= a
-    
+
     or, writing it in a way that can be easily implemented in this function,
-    
-        max(a,b,c) <= a + b + c - max(a,b,c)    
-    
+
+        max(a, b, c) <= a + b + c - max(a, b, c)
+
     (Note that equality only occurs in the case of a triangle with zero area,
     which effectively forms a straight line with one 180 and two 0 degree angles,
     thereby demonstrating that the shortest distance between two points is a
     straight line in Euclidean space.)
-    
-    See https://en.wikipedia.org/wiki/Triangle_inequality 
+
+    See https://en.wikipedia.org/wiki/Triangle_inequality
     for a more detailed explanation, and mathematical proofs at
     https://proofwiki.org/wiki/Triangle_Inequality
     https://proofwiki.org/wiki/Sum_of_Two_Sides_of_Triangle_Greater_than_Third_Side
     """
-    return max(a,b,c) <= a + b + c - max(a,b,c)
+    return max(a, b, c) <= a + b + c - max(a, b, c)
 
-def classifyTriangle(a,b,c):
-    if isTriangle(a,b,c):
-        #Python 3 has the statistics module and median function, but
-        #Python 2 doesn't so we just find it ourselves
-        mediannumber = sorted([a,b,c])[1]
-        
+
+def classifyTriangle(a, b, c):
+    if isTriangle(a, b, c):
+        # Python 3 has the statistics module and median function, but
+        # Python 2 doesn't so we just find it ourselves
+        mediannumber = sorted([a, b, c])[1]
+
         if a == b and b == c:
-            #Equilateral triangle if all sides are of equal length
+            # Equilateral triangle if all sides are of equal length
             return 'Equilateral'
         elif a == b or b == c or c == a:
-            #Isosceles triangle if any two sides are of equal length
-            if abs(min(a,b,c)**2 + mediannumber**2 - max(a,b,c)**2) < 0.0000001:
-                #Currently testing to an accuracy of 1E-07
-                #This can be changed later if needed
+            # Isosceles triangle if any two sides are of equal length
+            if abs(min(a, b, c) ** 2 + mediannumber ** 2 - max(a, b, c) ** 2) < 0.0000001:
+                # Currently testing to an accuracy of 1E-07
+                # This can be changed later if needed
                 return 'IsoscelesRight'
             else:
                 return 'Isosceles'
         else:
-            #Scalene triangles have no specific relationship between sides
-            if abs(min(a,b,c)**2 + mediannumber**2 - max(a,b,c)**2) < 0.0000001:
-                #Currently testing to an accuracy of 1E-07
-                #This can be changed later if needed
+            # Scalene triangles have no specific relationship between sides
+            if abs(min(a, b, c) ** 2 + mediannumber ** 2 - max(a, b, c) ** 2) < 0.0000001:
+                # Currently testing to an accuracy of 1E-07
+                # This can be changed later if needed
                 return 'ScaleneRight'
             else:
                 return 'Scalene'
     else:
         return 'NotATriangle'
 
+
 if __name__ == "__main__":
-    print "isTriangle(3,4,5) = " + str(isTriangle(3,4,5))
-    print "isTriangle(2,2,1) = " + str(isTriangle(2,2,1))
-    
-    print "isTriangle(1,4,6) = " + str(isTriangle(1,4,6))
-    print "isTriangle(3,3,9) = " + str(isTriangle(3,3,9))
-    
-    print "classifyTriangle(3,4,5) = " + classifyTriangle(3,4,5)
-    print "classifyTriangle(1,2,3) = " + classifyTriangle(1,2,3)
+    print "isTriangle(3,4,5) = " + str(isTriangle(3, 4, 5))
+    print "isTriangle(2,2,1) = " + str(isTriangle(2, 2, 1))
+
+    print "isTriangle(1,4,6) = " + str(isTriangle(1, 4, 6))
+    print "isTriangle(3,3,9) = " + str(isTriangle(3, 3, 9))
+
+    print "classifyTriangle(3,4,5) = " + classifyTriangle(3, 4, 5)
+    print "classifyTriangle(1,2,3) = " + classifyTriangle(1, 2, 3)
+    print "classifyTriangle(1,1,sqrt(2) = " + classifyTriangle(1, 1, sqrt(2))
--- test_homework01.py
+++ test_homework01.py
@@ -5,52 +5,54 @@
 #############################
 import unittest
 from homework01 import *
-from math import sqrt
+
 
 class TestHomework01(unittest.TestCase):
-    
+
     def setUp(self):
         pass
-    
+
     def test_isTriangleTrue(self):
-        #Test some numbers that form triangles
-        self.assertTrue(isTriangle(3,4,5))
-        self.assertTrue(isTriangle(2,2,1))
-        self.assertTrue(isTriangle(1,2,3))
-        self.assertTrue(isTriangle(8,3,10))
-        self.assertTrue(isTriangle(7,2,8))
-    
+        # Test some numbers that form triangles
+        self.assertTrue(isTriangle(3, 4, 5))
+        self.assertTrue(isTriangle(2, 2, 1))
+        self.assertTrue(isTriangle(1, 2, 3))
+        self.assertTrue(isTriangle(8, 3, 10))
+        self.assertTrue(isTriangle(7, 2, 8))
+
     def test_isTriangleFalse(self):
-        #Test some numbers that don't form triangles
-        self.assertFalse(isTriangle(1,4,6))
-        self.assertFalse(isTriangle(3,3,9))        
-        self.assertFalse(isTriangle(1,2,5))
-        self.assertFalse(isTriangle(8,2,12))
-        self.assertFalse(isTriangle(5,2,9))
-    
+        # Test some numbers that don't form triangles
+        self.assertFalse(isTriangle(1, 4, 6))
+        self.assertFalse(isTriangle(3, 3, 9))
+        self.assertFalse(isTriangle(1, 2, 5))
+        self.assertFalse(isTriangle(8, 2, 12))
+        self.assertFalse(isTriangle(5, 2, 9))
+
     def test_classifyTriangle_NotATriangle(self):
-        #Test some non-triangles
-        self.assertEqual(classifyTriangle(1,4,6),'NotATriangle')
-    
+        # Test some non-triangles
+        self.assertEqual(classifyTriangle(1, 4, 6), 'NotATriangle')
+
     def test_classifyTriangle_Equilateral(self):
-        #Test some Equilateral Triangles
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
-    
+        # Test some Equilateral Triangles
+        self.assertEqual(classifyTriangle(1, 1, 1), 'Equilateral')
+
     def test_classifyTriangle_Isosceles(self):
-        #Test some Isosceles Triangles
-        self.assertEqual(classifyTriangle(2, 2, 1),'Isosceles')
-    
+        # Test some Isosceles Triangles
+        self.assertEqual(classifyTriangle(2, 2, 1), 'Isosceles')
+
     def test_classifyTriangle_IsoscelesRight(self):
-        #Test some Isosceles Right Triangles
-        self.assertEqual(classifyTriangle(sqrt(2),sqrt(2),2),'IsoscelesRight')
-    
+        # Test some Isosceles Right Triangles
+        self.assertEqual(classifyTriangle(
+            sqrt(2), sqrt(2), 2), 'IsoscelesRight')
+
     def test_classifyTriangle_Scalene(self):
-        #Test some Scalene Triangles
-        self.assertEqual(classifyTriangle(1,2,3),'Scalene')
-    
+        # Test some Scalene Triangles
+        self.assertEqual(classifyTriangle(1, 2, 3), 'Scalene')
+
     def test_classifyTriangle_ScaleneRight(self):
-        #Test some Scalene Right Triangles
-        self.assertEqual(classifyTriangle(3,4,5),'ScaleneRight')
-    
+        # Test some Scalene Right Triangles
+        self.assertEqual(classifyTriangle(3, 4, 5), 'ScaleneRight')
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
